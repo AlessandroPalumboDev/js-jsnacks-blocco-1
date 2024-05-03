259394347667c624b5d748c9b2e61f1097df8b46
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-03 12:53:43 +0200

    [FIX] Cambio parseInt in Number

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,19 +1,19 @@
 "use strict"
 
 // --creo 10 prompt
-let numero1 = parseInt( prompt ('inserisci un numero'));
-let numero2 = parseInt( prompt ('inserisci un numero'));
-let numero3 = parseInt( prompt ('inserisci un numero'));
-let numero4 = parseInt( prompt ('inserisci un numero'));
-let numero5 = parseInt( prompt ('inserisci un numero'));
-let numero6 = parseInt( prompt ('inserisci un numero'));
-let numero7 = parseInt( prompt ('inserisci un numero'));
-let numero8 = parseInt( prompt ('inserisci un numero'));
-let numero9 = parseInt( prompt ('inserisci un numero'));
-let numero10 = parseInt( prompt ('inserisci un numero'));
+let numero1 = Number( prompt ('inserisci un numero'));
+let numero2 = Number( prompt ('inserisci un numero'));
+let numero3 = Number( prompt ('inserisci un numero'));
+let numero4 = Number( prompt ('inserisci un numero'));
+let numero5 = Number( prompt ('inserisci un numero'));
+let numero6 = Number( prompt ('inserisci un numero'));
+let numero7 = Number( prompt ('inserisci un numero'));
+let numero8 = Number( prompt ('inserisci un numero'));
+let numero9 = Number( prompt ('inserisci un numero'));
+let numero10 = Number( prompt ('inserisci un numero'));
 
 // --sommo le variabili
-const somma = (numero1 + numero2 + numero3 + numero4 +numero5 + numero6 + numero7 + numero8 + numero9 + numero10)
+const somma = Number(numero1 + numero2 + numero3 + numero4 +numero5 + numero6 + numero7 + numero8 + numero9 + numero10)
 
 // --stampo il risultato
 console.log(somma)
