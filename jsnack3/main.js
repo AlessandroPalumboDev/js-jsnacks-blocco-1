7eb141d60e2aee093bd0cdddbdbf3e2c38996a73
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-03 12:48:53 +0200

    jsnack3

---
+++ jsnack3/main.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// --creo 10 prompt
+let numero1 = parseInt( prompt ('inserisci un numero'));
+let numero2 = parseInt( prompt ('inserisci un numero'));
+let numero3 = parseInt( prompt ('inserisci un numero'));
+let numero4 = parseInt( prompt ('inserisci un numero'));
+let numero5 = parseInt( prompt ('inserisci un numero'));
+let numero6 = parseInt( prompt ('inserisci un numero'));
+let numero7 = parseInt( prompt ('inserisci un numero'));
+let numero8 = parseInt( prompt ('inserisci un numero'));
+let numero9 = parseInt( prompt ('inserisci un numero'));
+let numero10 = parseInt( prompt ('inserisci un numero'));
+
+// --sommo le variabili
+const somma = (numero1 + numero2 + numero3 + numero4 +numero5 + numero6 + numero7 + numero8 + numero9 + numero10)
+
+// --stampo il risultato
+console.log(somma)
+
