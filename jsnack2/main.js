3a6b7646a0ce46ff198dfea0d6c783d2d50e6e66
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-03 12:05:50 +0200

    [FIX] Aggiunto spazio tra le parole in output

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -5,12 +5,11 @@ const parola1 = prompt("Inserisci parola 1");
 const parola2 = prompt("Inserisci parola 2");
 // Se la lunghezza della prima parola è più corta la stampo per prima
 if(parola1.length < parola2.length){
-    console.log(parola1 + parola2)
-// Se la lunghezza dell aprima parola è più lunga la stampo per seconda
+    console.log(`${parola1} ${parola2}`)
+// Se la lunghezza della prima parola è più lunga la stampo per seconda
 } else if(parola1.length > parola2.length){
-    console.log(parola2 + parola1)
-}
+    console.log(`${parola2} ${parola1}`)
 // Se la lunghezza delle due parole è identica stampa in ordine di inserimento
-else{
-    console.log(parola1 + parola2)
+}else{
+    console.log(`${parola1} ${parola2}`)
 }
