83897e5bb130c62419a1d9971b27569b20e2bf4c
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-03 20:59:21 +0200

    [UPD] Calcolo della somma

---
+++ jsnack9/main.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// RIUTILIZZO IL CODICE DI JSNACK3 PER LA SOMMA
+
+// variabile della somma iniziale a 0 
+let somma = 0;
+
+// Quanti numeri sono richiesti
+const count = 10
+
+// richieste tramite prompt
+for (let i = 1; i <= count; i++){
+    const numero = Number(prompt(`Digita il ${i}° numero di ${count}`))
+
+    // verifico che sia sempre un numero
+    if (!isNaN (numero)){
+        somma += numero;
+    }else {
+        console.log(`Il ${i}° dato inserito non è un numero`);
+    
+        // rimuovo dal contatore il valore che non è un numero
+        i--;
+    };
+};
+console.log(`La somma totale dei ${count} numeri è ${somma}`)
