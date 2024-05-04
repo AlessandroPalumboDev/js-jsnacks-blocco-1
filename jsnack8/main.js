fa8f40bd29e4f72b8a40fff4bec875c9dc684846
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-04 17:05:33 +0200

    [UPD] Controlli inserimento dati corretto

--- jsnack8/main.js
+++ jsnack8/main.js
@@ -8,3 +8,24 @@ const lunghezzaMassima = 4;
 
 // richiesta inserimento numero
 const numero = prompt(`Inserisci un numero di ${lunghezzaMassima} cifre`);
+
+ // verifico che sia sempre un numero 
+ if (!isNaN (numero)){
+
+    // mi assicuro siano solo e al massimo {lunghezzaMassima} cifre
+    if (numero.length === lunghezzaMassima){
+
+
+
+
+    // in caso sia un numero più lungo o più corto della lunghezza massima
+    } else{
+        console.log(`Puoi inserire solo un numero di ${lunghezzaMassima} cifre!`)
+        alert(`Puoi inserire solo un numero di ${lunghezzaMassima} cifre!`);
+    };
+
+// in caso non sia un numero
+}else {
+console.log(`Non puoi digitare caratteri diversi da numeri`);
+alert(`Non puoi digitare caratteri diversi da numeri`);
+};
