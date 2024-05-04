3af3869554cedec7084ee6a6b73e1851f51497ab
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-04 17:15:19 +0200

    [UPD] Somma e stampa

--- jsnack8/main.js
+++ jsnack8/main.js
@@ -9,28 +9,34 @@ const lunghezza = 4;
 // richiesta inserimento numero
 const numero = prompt(`Inserisci un numero di ${lunghezza} cifre`);
 
- // verifico che sia sempre un numero 
- if (!isNaN (numero)){
+// verifico che sia sempre un numero 
+if (!isNaN(numero)) {
 
     // mi assicuro siano solo e al massimo {lunghezza} cifre
-    if (numero.length === lunghezza){
+    if (numero.length === lunghezza) {
 
-         // smonto il numero inserito creando un array "momentaneo"
-         let numeroSmontato = numero.split('');
-         console.log(numeroSmontato)
- 
+        // smonto il numero inserito creando un array "momentaneo"
+        const numeroSmontato = numero.split('');
 
+        // Ciclo per sommare gli elementi dell'array
+        for (let i = 0; i < numeroSmontato.length; i++) {
 
+            // per fare la somma devo prima trasformare gli elementi dell' array "momentaneo" in numeri
+            somma += Number(numeroSmontato[i]);
+        }
 
+        // stampo il risultato
+        console.log(`La somma delle cifre di ${numero} è ${somma}`);
+        alert(`La somma delle cifre di ${numero} è ${somma}`);
 
-    // in caso sia un numero più lungo o più corto della lunghezza 
-    } else{
+        // in caso sia un numero più lungo o più corto della lunghezza 
+    } else {
         console.log(`Puoi inserire solo un numero di ${lunghezza} cifre!`)
         alert(`Puoi inserire solo un numero di ${lunghezza} cifre!`);
     };
 
-// in caso non sia un numero
-}else {
-console.log(`Non puoi digitare caratteri diversi da numeri`);
-alert(`Non puoi digitare caratteri diversi da numeri`);
+    // in caso non sia un numero
+} else {
+    console.log(`Non puoi digitare caratteri diversi da numeri`);
+    alert(`Non puoi digitare caratteri diversi da numeri`);
 };
