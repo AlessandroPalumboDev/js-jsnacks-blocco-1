d447a89a751559267d06e15bd439ca4cdc156d90
AlessandroPalumboDev <alessandropalumbo.dev@gmail.com>
2024-05-04 16:54:48 +0200

    [UPD] Create variabili e prompt

---
+++ jsnack8/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// variabile somma
+let somma = 0;
+
+// variabile della lunghezza massima del numero inserito
+const lunghezzaMassima = 4;
+
+// richiesta inserimento numero
+const numero = prompt(`Inserisci un numero di ${lunghezzaMassima} cifre`);
